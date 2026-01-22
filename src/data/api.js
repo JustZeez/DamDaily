27490156f7f3fb351db08606b4c0a2a18631cf1b
JustZeez <justzeez65@gmail.com>
2026-01-22 22:17:27 +0100

    Installed React icons

--- src/data/api.js
+++ src/data/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const api = "http://localhost:4000/api";
+// const api = "http://localhost:4000/api";
+const api = "https://damdailybackend.onrender.com/api";
 
 const registerUser = async (data) => {
     return await axios.post(`${api}/auth/register`,data)
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ import {
   FaLockOpen,
   FaGem,
   FaInfinity,
-} from "react-icons/fa";
+} from "react-icons/fa/index.js";
 
 export default function Home() {
   const [activeReaders, setActiveReaders] = useState(0);
